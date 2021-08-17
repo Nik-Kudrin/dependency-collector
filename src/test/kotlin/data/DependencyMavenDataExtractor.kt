d1343487d353cabea5f3e15d0acc608bc3e8cd98
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2021-08-17 10:14:34 +0300

    handle network errors / bans

--- src/test/kotlin/data/DependencyMavenDataExtractor.kt
+++ src/test/kotlin/data/DependencyMavenDataExtractor.kt
@@ -3,6 +3,8 @@ package data
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.io.*
@@ -15,6 +17,8 @@ import java.net.http.HttpResponse
 import java.nio.file.Path
 import kotlin.random.Random
 import kotlin.random.nextInt
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 private val LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
@@ -41,6 +45,8 @@ private data class MavenSearchModel(
 class DependencyMavenDataExtractor {
     private lateinit var cleanBaseDependenciesFile: File
     private lateinit var librariesFromMavenCentral: File
+    private val countOfExceptionThreshold = 3
+    private var countOfHttpException: Int = 0
 
     private fun getDistinctPackages(packages: Iterable<String>): Set<String> {
         val groupedPackages = packages
@@ -79,9 +85,7 @@ class DependencyMavenDataExtractor {
 //        println(uniquePackages.toString())
         cleanBaseDependenciesFile = File.createTempFile("Clean_Base_Dependencies_List", ".txt")
 
-        BufferedWriter(FileWriter(cleanBaseDependenciesFile)).use { writer ->
-            uniquePackages.forEach { writer.write(it + System.lineSeparator()) }
-        }
+        writeIterableToFile(uniquePackages, cleanBaseDependenciesFile)
     }
 
     @Test
@@ -117,64 +121,118 @@ class DependencyMavenDataExtractor {
 
 //                println(packages.toString())
         librariesFromMavenCentral = File.createTempFile("Libraries_List_From_Maven_Central", ".txt")
-
-        BufferedWriter(FileWriter(librariesFromMavenCentral)).use { writer ->
-            packages.forEach { writer.write(it + System.lineSeparator()) }
-        }
+        writeIterableToFile(packages, librariesFromMavenCentral)
 
         println("Packages has been written to file: ${librariesFromMavenCentral.path}")
     }
 
+    private fun readFileToSet(filePath: File): Set<String> = readFileToSet(filePath.path)
+
+    private fun readFileToSet(filePath: String): Set<String> {
+        BufferedReader(FileReader(filePath)).use {
+            return it.readLines().toSet()
+        }
+    }
+
+    private fun writeIterableToFile(iterable: Iterable<String>, file: File) {
+        if (file.exists()) {
+            file.delete()
+            file.createNewFile()
+        }
+
+        BufferedWriter(FileWriter(file)).use { writer ->
+            iterable.forEach { writer.write(it + System.lineSeparator()) }
+        }
+    }
+
+    @ExperimentalTime
     @Test
-    fun mergePackages() {
-        extractAndCleanupPackageDataFromCommonListOfPackages()
-        getMavenCentralPackagesList()
+    fun mergePackagesCached() {
+        //        extractAndCleanupPackageDataFromCommonListOfPackages()
+//        getMavenCentralPackagesList()
 
-        val baseDependencies = mutableSetOf<String>()
-        val parsedDependencies = mutableSetOf<String>()
+        cleanBaseDependenciesFile = Path.of("/tmp/Clean_Base_Dependencies_List8591417797809616084.txt").toFile()
+        librariesFromMavenCentral = Path.of("/tmp/Libraries_List_From_Maven_Central13161733866809064272.txt").toFile()
 
-        BufferedReader(FileReader(cleanBaseDependenciesFile)).use {
-            baseDependencies.addAll(it.readLines())
-        }
+        val baseDependencies = readFileToSet(cleanBaseDependenciesFile)//.take(3000).toSet()
+        val parsedDependencies = readFileToSet(librariesFromMavenCentral)//.take(3000).toSet()
 
-        BufferedReader(FileReader(librariesFromMavenCentral)).use {
-            parsedDependencies.addAll(it.readLines())
-        }
+        // store partially processed packages
 
-        val uniquePackages = getDistinctPackages(baseDependencies.toMutableSet().also { it.addAll(parsedDependencies) })
+        val notFoundPackages = mutableSetOf<String>()
+        val existedPackages = mutableSetOf<String>()
+
+        var inputSet = baseDependencies.plus(parsedDependencies)
+
+        val totalNotFoundFile = File.createTempFile("TOTAL_NOT_FOUND", ".txt")
+        val totalExistFile = File.createTempFile("TOTAL_EXIST", ".txt")
+
+        do {
+            countOfHttpException = 0
+            val (packagesNotFoundFilePath, existedPackagesFilePath) = mergePackages(emptySet(), inputSet)
+
+            notFoundPackages.addAll(readFileToSet(packagesNotFoundFilePath))
+            existedPackages.addAll(readFileToSet(existedPackagesFilePath))
+
+            writeIterableToFile(notFoundPackages, totalNotFoundFile)
+            println("SUBTOTAL ${notFoundPackages.size} NOT FOUND PACKAGES FILE: ${totalNotFoundFile.path}")
+
+            writeIterableToFile(existedPackages, totalExistFile)
+            println("SUBTOTAL ${existedPackages.size} EXIST PACKAGES FILE: ${totalExistFile.path}")
+
+            runBlocking { delay(Duration.seconds(60)) }
+
+            inputSet = inputSet.minus(notFoundPackages).minus(existedPackages)
+            println("Left to check packages: ${inputSet.size}")
+
+        } while (inputSet.isNotEmpty())
+
+        println("====== THE END ============")
+    }
+
+    @ExperimentalTime
+//    @Test
+    fun mergePackages(firstSet: Set<String>, secondSet: Set<String>): Pair<String, String> {
+
+        val uniquePackages = getDistinctPackages(firstSet.toMutableSet().also { it.addAll(secondSet) })
             .toMutableSet()
 
         println("Raw packages count: ${uniquePackages.size}")
 
-        val packagesDoesntExist = mutableSetOf<String>()
-        uniquePackages.forEachIndexed { index, item ->
-            if (index % 100 == 0) // every N request
+        val packagesNotFound = mutableSetOf<String>()
+        val existedPackages = mutableSetOf<String>()
+
+        var index = 0
+        for (item in uniquePackages) {
+            if (index++ % 100 == 0) // every N request
                 println("Checking package existence: ${index * 100.0 / uniquePackages.size} %")
+
+            if (countOfHttpException >= countOfExceptionThreshold) {
+                System.err.println("Http exception threshold in ${countOfExceptionThreshold} requests was reached")
+                break
+            }
 
             if (!checkIsDependencyAvailable(item)) {
 //                println("Package $item doesn't exist")
-                packagesDoesntExist.add(item)
+                packagesNotFound.add(item)
+            } else {
+                existedPackages.add(item)
             }
         }
 
-        uniquePackages.removeAll(packagesDoesntExist)
-
-        println("Count of packages, that weren't found ${packagesDoesntExist.size}. Packages, that exist: ${uniquePackages.size}")
+        println("Count of packages, that weren't found ${packagesNotFound.size}. Packages, that exist: ${existedPackages.size}")
 
         val notExistPackagesFile = File.createTempFile("Not_Existed_Packages", ".txt")
-        BufferedWriter(FileWriter(notExistPackagesFile)).use { writer ->
-            packagesDoesntExist.forEach { writer.write(it + System.lineSeparator()) }
-        }
+        writeIterableToFile(packagesNotFound, notExistPackagesFile)
         println("Packages, that weren't found on Maven Central has been written to file: ${notExistPackagesFile.path}")
 
 
-        val outputFile = File.createTempFile("Libraries_Final_List", ".txt")
+        val finalList = File.createTempFile("Libraries_Final_List", ".txt")
+        writeIterableToFile(existedPackages.shuffled(), finalList)
 
-        BufferedWriter(FileWriter(outputFile)).use { writer ->
-            uniquePackages.shuffled().forEach { writer.write(it + System.lineSeparator()) }
-        }
+        println("List with final packages (that do exist) has been written to file: ${finalList.path}")
 
-        println("List with final packages (that do exist) has been written to file: ${outputFile.path}")
+        return Pair(notExistPackagesFile.path, finalList.path)
     }
 
     @Test
@@ -202,14 +260,22 @@ class DependencyMavenDataExtractor {
             .append("/$packageName-$version.jar")
 
         val artifactUrl = URL(urlStringBuilder.toString())
+
+
         return try {
             val responseCode = (artifactUrl.openConnection() as HttpURLConnection).responseCode
             when (responseCode) {
                 200 -> true
-                else -> false
+                404 -> false
+                else -> {
+                    System.err.println("Unexpected request status code: $responseCode")
+                    false
+                }
             }
-
         } catch (e: Exception) {
+            countOfHttpException++
+//            e.printStackTrace()
+            System.err.println("Http failure encountered")
             false
         }
     }
